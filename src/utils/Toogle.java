742e44d2a22a562ca0fd828c797174b1894168d2
Marc Woodyard <MarcWoodyard@users.noreply.github.com>
2017-10-09 18:34:14 -0700

    Update Toogle.java

--- src/utils/Toogle.java
+++ src/utils/Toogle.java
@@ -8,11 +8,11 @@ public class Toogle {
         runTime.exec("gpio pwm-ms");
         runTime.exec("gpio pwmc 192");
         runTime.exec("gpio pwmr 2000");
-		    runTime.exec("gpio pwm 1 60");
-		    Thread.sleep(600);
+	runTime.exec("gpio pwm 1 60");
+	Thread.sleep(600);
         runTime.exec("gpio pwm 1 55");
-		    Thread.sleep(600);
-		    runTime.exec("gpio pwm 1 60");
+	Thread.sleep(600);
+	runTime.exec("gpio pwm 1 60");
     } catch(Exception e) {
         e.printStackTrace();
     }
