15a3215ba179cdf21e04da9d5c293131dd8fc2bc
Marc Woodyard <MarcWoodyard@users.noreply.github.com>
2018-04-15 01:57:29 -0700

    Servo Controller Update

--- src/sensors/Servo.java
+++ src/sensors/Servo.java
@@ -12,10 +12,7 @@ public class Servo {
 	private int servoMax = config.getSwitchButton();
 	private int servoResting = config.getRestingPosition();
 	private int servoTimeout = config.getServoSleep();
-
-	// Servo
 	private boolean servoStatus = false;
-	private Runtime runTime = Runtime.getRuntime();
 
 	// Logging
 	private Logger log = new Logger();
@@ -31,21 +28,23 @@ public class Servo {
 	* Turns AC on/off.
 	*/
 	public void switchAC() {
-    try {
-			 runTime.exec("gpio mode 100 pwm");
-			 runTime.exec("gpio pwm-ms");
-			 runTime.exec("gpio pwmc 192");
-			 runTime.exec("gpio pwmr 2000");
-			 runTime.exec("gpio pwm 1 " + this.servoResting);
-			 Thread.sleep(this.servoTimeout);
-			 runTime.exec("gpio pwm 1 " + this.servoMax);
-			 Thread.sleep(this.servoTimeout);
-			 runTime.exec("gpio pwm 1 " + this.servoResting);
-    } catch(Exception e) {
+	    try {
+			Runtime runTime = Runtime.getRuntime();
+			runTime.exec("gpio mode 1 pwm");
+			runTime.exec("gpio pwm-ms");
+			runTime.exec("gpio pwmc 192");
+			runTime.exec("gpio pwmr 2000");
+			runTime.exec("gpio pwm 1 " + this.servoResting); // Center
+			Thread.sleep(this.servoTimeout);
+			runTime.exec("gpio pwm 1 " + this.servoMax); // Turn on/off
+			Thread.sleep(this.servoTimeout);
+			runTime.exec("gpio pwm 1 " + this.servoResting); // Center
+			Thread.sleep(this.servoTimeout);
+	    } catch(Exception e) {
 			this.log.add("[ERROR]", "Exception occured: " + e.getMessage());
 			this.log.alert("AC Controller Servo Error",
 					"The AC controller encountered a servo motor error.\n\nException occured:\n" + e.getMessage());
-    }
+	    }
 
 		// Update Servo State
 		this.servoStatus = !this.servoStatus;
